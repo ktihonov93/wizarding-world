733c4ef1058de8c5d8dfafb999d5213822927768
ktihonov93 <k_tihonov@mail.ru>
2022-07-22 03:13:26 +0300

    add search in char list

--- src/Containers/CharacterList/CharacterList.jsx
+++ src/Containers/CharacterList/CharacterList.jsx
@@ -1,31 +1,57 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import LoadingSpinner from "../../Components/LoadingSpinner";
+import {useDebounce} from 'use-debounce';
+//import LoadingSpinner from "../../Components/LoadingSpinner";
 import CharacterCard from "../CharacterCard";
 import "./CharacterList.css";
 
 export const CharacterList = () => {
   const [characters, setCharacters] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [offset, setOffset] = useState(0);
+  //const [loading, setLoading] = useState(true);
+  const cardsOnPage = 12;
+  const [search, setSearch] = useState("");
 
-  useEffect(() => {
+  const fetchData = () => {
     axios
       .get(`https://hp-api-marcosmarp.herokuapp.com/api/characters`)
       .then((res) => {
-        setCharacters(res.data.slice(0, 12));
-        setLoading(false);
-        //console.log(res);
+        setCharacters(
+          res.data
+            .filter((character) =>
+              character.name.toLowerCase().includes(searchDebounced.toLowerCase())
+            )
+            .slice(0, cardsOnPage)
+        );
+        //setLoading(false);
       });
-  }, [offset]);
+  };
 
-  return loading ? (
-    <LoadingSpinner />
-  ) : (
-    <section className="CharacterList">
-      {characters.map((el) => (
-        <CharacterCard key={el.id} id={el.id} name={el.name} image={el.image} />
-      ))}
-    </section>
+  const [searchDebounced] = useDebounce(search, 250)
+
+  useEffect(() => {
+    searchDebounced && fetchData()
+  }, [searchDebounced]);
+
+  return (
+    <div>
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearch(e.target.value.trim())}
+      />
+
+      <section className="CharacterList">
+        {characters &&
+          characters.map((el) => (
+            <CharacterCard
+              key={el.id}
+              id={el.id}
+              name={el.name}
+              image={el.image}
+            />
+          ))}
+      </section>
+    </div>
   );
 };

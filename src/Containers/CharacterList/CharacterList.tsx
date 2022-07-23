b211debea274ebf65f00b11d376e1f1b11e89888
ktihonov93 <k_tihonov@mail.ru>
2022-07-23 13:27:09 +0300

    change type in character list

--- src/Containers/CharacterList/CharacterList.tsx
+++ src/Containers/CharacterList/CharacterList.tsx
@@ -1,18 +1,21 @@
 import CharacterCard from "../CharacterCard";
 import "./CharacterList.css";
 
-type Characters = [
-  {
-    name: string;
-    image: string;
-    id: string;
-  }
-];
+type Characters = {
+  characters: [
+    {
+      name: string;
+      image: string;
+      id: string;
+    }
+  ];
+};
 
 export const CharacterList = (characters: Characters) => {
+  console.log(characters.characters);
   return (
     <section className="CharacterList">
-      {characters.map((el) => (
+      {characters.characters.map((el) => (
         <CharacterCard key={el.id} id={el.id} name={el.name} image={el.image} />
       ))}
     </section>

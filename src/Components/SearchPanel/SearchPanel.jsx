461e492d0e6bc35c9410938d9553815b805cc5a0
ktihonov93 <k_tihonov@mail.ru>
2022-08-03 21:46:30 +0300

    add links over history items

--- src/Components/SearchPanel/SearchPanel.jsx
+++ src/Components/SearchPanel/SearchPanel.jsx
@@ -1,18 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { history } from "../../slices/history";
 import "./SearchPanel.css";
 
-export const SearchPanel = ({callback}) => {
+export const SearchPanel = ({ callback }) => {
   const dispatch = useDispatch();
+  const historyItem = useSelector((state) => state.history?.historyItem);
 
   return (
     <input
       className="search__input"
       type="text"
+      defaultValue={historyItem? historyItem : ""}
       placeholder="Search"
       onChange={(e) => {
-        callback(e.target.value.trim())
-        dispatch(history(e.target.value.trim() ))
+        callback(e.target.value.trim());
+        dispatch(history(e.target.value.trim()));
       }}
     />
   );

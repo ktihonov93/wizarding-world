51109ab975f97699aa0156eec1817e2194be0261
ktihonov93 <k_tihonov@mail.ru>
2022-08-05 03:18:12 +0300

    add RTK-Query

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://hp-api-marcosmarp.herokuapp.com/api/characters" }),
+  endpoints: (builder) => ({
+    getCharacter: builder.query({
+        query: id => `/${id}`
+      })
+  }),
+});
+
+export const { useGetCharacterQuery } = apiSlice;

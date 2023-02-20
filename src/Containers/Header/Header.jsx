c2ffee7b75239ef8e8b621a4da060c33c875db9e
ktihonov93 <https://ktihonov93@github.com>
2023-02-20 14:47:03 +0300

    change link in logout

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -40,7 +40,7 @@ export const Header = () => {
             </Link>
           </li>
           <li className="nav-item">
-            <a href="/login" className="nav-link" onClick={logOut}>
+            <a href="/wizarding-world/login" className="nav-link" onClick={logOut}>
               LogOut
             </a>
           </li>
